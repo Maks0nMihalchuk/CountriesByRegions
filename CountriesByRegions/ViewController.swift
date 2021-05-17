61f1ba2a96332ef3e3abdbd27e31e817555b6fac
Maks0nMihalchuk <maks.mihalchuk.2000@gmail.com>
2021-05-17 15:20:37 +0300

    Initial Commit

---
+++ CountriesByRegions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountriesByRegions
+//
+//  Created by MaksOn on 17.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
