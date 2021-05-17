77023703eb1ce34c04a5e5d54e3ebbda06b62102
Maks0nMihalchuk <maks.mihalchuk.2000@gmail.com>
2021-05-17 16:47:24 +0300

    did project struct

---
+++ CountriesByRegions/Screens/CountriesByRegionsScreen/Controller/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CountriesByRegions
+//
+//  Created by MaksOn on 17.05.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
